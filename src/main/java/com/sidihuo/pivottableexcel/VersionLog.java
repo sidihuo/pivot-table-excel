bc34c4dac02d966842f5ecb71f0ce00297d48876
yanggangjie <Cpic@1234>
2023-08-01 19:03:13 +0800

    init

---
+++ src/main/java/com/sidihuo/pivottableexcel/VersionLog.java
@@ -0,0 +1,12 @@
+package com.sidihuo.pivottableexcel;
+
+/**
+ * @Description
+ * @Date 2023/2/22 18:44
+ * @Created by yanggangjie
+ */
+public class VersionLog {
+
+    public static String JAR_BUILD_TIME = "2023-0801-1900";
+
+}
---
+++ src/test/java/com/sidihuo/pivottableexcel/test/TestA.java
@@ -0,0 +1,9 @@
+package com.sidihuo.pivottableexcel.test;
+
+/**
+ * @Description
+ * @Date 2023/7/25 15:36
+ * @Created by yanggangjie
+ */
+public class TestA {
+}
